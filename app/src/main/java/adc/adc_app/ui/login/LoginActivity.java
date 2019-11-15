cd8e4ba7bc9f4e0bf3eb1ced20b047f8b227a9f6
amitej <v.amitej@gmail.com>
2019-11-15 11:26:11 -0500

    Renamed LoginActivity

--- app/src/main/java/adc/adc_app/ui/login/LoginActivity.java
+++ app/src/main/java/adc/adc_app/ui/login/LoginActivity.java
@@ -22,7 +22,7 @@ import adc.adc_app.R;
 import adc.adc_app.ui.login.LoginViewModel;
 import adc.adc_app.ui.login.LoginViewModelFactory;
 
-public class SignUpPage extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
 
