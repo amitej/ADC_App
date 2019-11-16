2dcb12a6c363700500b71ff517dac76672287ea2
amitej <v.amitej@gmail.com>
2019-11-15 20:55:41 -0500

    set action bar text and background color. added to doula questions page

--- ADC_App/app/src/main/java/edu/gatech/adc/adc_app/MainActivity.java
+++ ADC_App/app/src/main/java/edu/gatech/adc/adc_app/MainActivity.java
@@ -2,13 +2,27 @@ package edu.gatech.adc.adc_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialButton requestDoula;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestDoula = findViewById(R.id.request_doula_button);
+        requestDoula.setOnClickListener(view -> openDoulaQuestionsActivity());
+    }
+
+    private void openDoulaQuestionsActivity() {
+        Intent intent = new Intent(this, DoulaQuestionsActivity.class);
+        startActivity(intent);
     }
 }
