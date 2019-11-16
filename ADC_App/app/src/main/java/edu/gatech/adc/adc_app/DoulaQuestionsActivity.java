abe8a014cf6201244cd75be2e0c3befc2c733772
amitej <v.amitej@gmail.com>
2019-11-16 15:43:12 -0500

    added functionality to some buttons for testing purposes

--- ADC_App/app/src/main/java/edu/gatech/adc/adc_app/DoulaQuestionsActivity.java
+++ ADC_App/app/src/main/java/edu/gatech/adc/adc_app/DoulaQuestionsActivity.java
@@ -2,13 +2,27 @@ package edu.gatech.adc.adc_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.button.MaterialButton;
+
 public class DoulaQuestionsActivity extends AppCompatActivity {
+
+    private MaterialButton prevButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doula_questions);
+
+        prevButton = findViewById(R.id.prev_button_doulaQ);
+        prevButton.setOnClickListener(view -> openCurrentBirthInfoActivity());
+    }
+
+    private void openCurrentBirthInfoActivity() {
+        Intent intent = new Intent(this, CurrentBirthInfoActivity.class);
+        startActivity(intent);
     }
 }
