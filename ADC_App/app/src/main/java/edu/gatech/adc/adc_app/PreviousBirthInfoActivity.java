6f5fdeb602ec537f893c5e4d6f93cccebea5faf2
shoun <snaeem6@gatech.edu>
2019-11-16 22:57:33 -0500

    Added the previous birth information pages and updated strings.xml file

---
+++ ADC_App/app/src/main/java/edu/gatech/adc/adc_app/PreviousBirthInfoActivity.java
@@ -0,0 +1,14 @@
+package edu.gatech.adc.adc_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreviousBirthInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_birth_info);
+    }
+}
